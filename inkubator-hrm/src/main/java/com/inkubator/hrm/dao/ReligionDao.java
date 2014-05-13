38e687e2febda1f750022e721691cd458ebfd728
rizkykojek <rizkykojek@gmail.com>
2014-05-13 11:17:51 +0700

    - Initial data

---
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/dao/ReligionDao.java
@@ -0,0 +1,12 @@
+package com.inkubator.hrm.dao;
+
+import com.inkubator.datacore.dao.IDAO;
+import com.inkubator.hrm.entity.Religion;
+
+/**
+*
+* @author rizkykojek
+*/
+public interface ReligionDao extends IDAO<Religion> {
+
+}
---
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/entity/Religion.java
@@ -0,0 +1,93 @@
+package com.inkubator.hrm.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="religion",catalog="hrm")
+public class Religion implements Serializable {
+
+	private Long id;
+	private String name;
+	private String createdBy;
+    private String updatedBy;
+    private Date createdOn;
+    private Date updatedOn;
+	
+	public Religion(){
+		
+	}
+	
+	public Religion(Long id){
+		this.id = id;
+	}
+	
+	public Religion(Long id, String name){
+		this.id = id;
+		this.name = name;
+	}
+	
+	@Id 
+    @Column(name="id", unique=true, nullable=false)
+    public Long getId() {
+        return this.id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name="name", unique=true, nullable=false)
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Column(name = "created_by", length = 45)
+    public String getCreatedBy() {
+        return this.createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    @Column(name = "updated_by", length = 45)
+    public String getUpdatedBy() {
+        return this.updatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_on", length = 19)
+    public Date getCreatedOn() {
+        return this.createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_on", length = 19)
+    public Date getUpdatedOn() {
+        return this.updatedOn;
+    }
+
+    public void setUpdatedOn(Date updatedOn) {
+        this.updatedOn = updatedOn;
+    }
+}
