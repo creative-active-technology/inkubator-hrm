84fd12cda8f02796b1995e94b0aa4de9cb9f3ae2
Deni Husni FR <Deni Husni FR@Positron>
2014-06-04 11:07:35 +0700

    Add Entity and Fix Relation Working Houre, ScheduleShift, WtGroupWorking

--- inkubator-hrm/src/main/java/com/inkubator/hrm/entity/WtWorkingHour.java
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/entity/WtWorkingHour.java
@@ -2,10 +2,13 @@ package com.inkubator.hrm.entity;
 // Generated May 21, 2014 4:59:40 PM by Hibernate Tools 3.6.0
 
 import java.util.Date;
-
+import java.util.HashSet;
+import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -20,7 +23,7 @@ public class WtWorkingHour implements java.io.Serializable {
 
     private Long id;
     private Integer version;
-    
+
     private String code;
     private String name;
     private String description;
@@ -46,6 +49,7 @@ public class WtWorkingHour implements java.io.Serializable {
     private Date createdOn;
     private String updatedBy;
     private Date updatedOn;
+    private Set<WtScheduleShift> wtScheduleShifts = new HashSet<WtScheduleShift>(0);
 
     public WtWorkingHour() {
     }
@@ -72,203 +76,203 @@ public class WtWorkingHour implements java.io.Serializable {
 
     public void setVersion(Integer version) {
         this.version = version;
-    }    
+    }
 
     @Column(name = "code", unique = true, nullable = false, length = 10)
     public String getCode() {
-		return code;
-	}
+        return code;
+    }
 
-	public void setCode(String code) {
-		this.code = code;
-	}
+    public void setCode(String code) {
+        this.code = code;
+    }
 
-	@Column(name = "name", unique = true, nullable = false)
-	public String getName() {
-		return name;
-	}
+    @Column(name = "name", unique = true, nullable = false)
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	@Column(name = "description")
-	public String getDescription() {
-		return description;
-	}
+    @Column(name = "description")
+    public String getDescription() {
+        return description;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	@Temporal(TemporalType.TIME)
+    @Temporal(TemporalType.TIME)
     @Column(name = "working_hour_begin", nullable = false, length = 10)
-	public Date getWorkingHourBegin() {
-		return workingHourBegin;
-	}
+    public Date getWorkingHourBegin() {
+        return workingHourBegin;
+    }
 
-	public void setWorkingHourBegin(Date workingHourBegin) {
-		this.workingHourBegin = workingHourBegin;
-	}
+    public void setWorkingHourBegin(Date workingHourBegin) {
+        this.workingHourBegin = workingHourBegin;
+    }
 
-	@Temporal(TemporalType.TIME)
+    @Temporal(TemporalType.TIME)
     @Column(name = "working_hour_end", nullable = false, length = 10)
-	public Date getWorkingHourEnd() {
-		return workingHourEnd;
-	}
+    public Date getWorkingHourEnd() {
+        return workingHourEnd;
+    }
 
-	public void setWorkingHourEnd(Date workingHourEnd) {
-		this.workingHourEnd = workingHourEnd;
-	}
+    public void setWorkingHourEnd(Date workingHourEnd) {
+        this.workingHourEnd = workingHourEnd;
+    }
 
-	@Temporal(TemporalType.TIME)
-	@Column(name = "max_hour", nullable = false, length = 10)
-	public Date getMaxHour() {
-		return maxHour;
-	}
+    @Temporal(TemporalType.TIME)
+    @Column(name = "max_hour", nullable = false, length = 10)
+    public Date getMaxHour() {
+        return maxHour;
+    }
 
-	public void setMaxHour(Date maxHour) {
-		this.maxHour = maxHour;
-	}
+    public void setMaxHour(Date maxHour) {
+        this.maxHour = maxHour;
+    }
 
-	@Column(name = "arrive_limit_begin", nullable = false, length = 3)
-	public Integer getArriveLimitBegin() {
-		return arriveLimitBegin;
-	}
+    @Column(name = "arrive_limit_begin", nullable = false, length = 3)
+    public Integer getArriveLimitBegin() {
+        return arriveLimitBegin;
+    }
 
-	public void setArriveLimitBegin(Integer arriveLimitBegin) {
-		this.arriveLimitBegin = arriveLimitBegin;
-	}
+    public void setArriveLimitBegin(Integer arriveLimitBegin) {
+        this.arriveLimitBegin = arriveLimitBegin;
+    }
 
-	@Column(name = "arrive_limit_end", nullable = false, length = 3)
-	public Integer getArriveLimitEnd() {
-		return arriveLimitEnd;
-	}
+    @Column(name = "arrive_limit_end", nullable = false, length = 3)
+    public Integer getArriveLimitEnd() {
+        return arriveLimitEnd;
+    }
 
-	public void setArriveLimitEnd(Integer arriveLimitEnd) {
-		this.arriveLimitEnd = arriveLimitEnd;
-	}
+    public void setArriveLimitEnd(Integer arriveLimitEnd) {
+        this.arriveLimitEnd = arriveLimitEnd;
+    }
 
-	@Column(name = "go_home_limit_begin", nullable = false, length = 3)
-	public Integer getGoHomeLimitBegin() {
-		return goHomeLimitBegin;
-	}
+    @Column(name = "go_home_limit_begin", nullable = false, length = 3)
+    public Integer getGoHomeLimitBegin() {
+        return goHomeLimitBegin;
+    }
 
-	public void setGoHomeLimitBegin(Integer goHomeLimitBegin) {
-		this.goHomeLimitBegin = goHomeLimitBegin;
-	}
+    public void setGoHomeLimitBegin(Integer goHomeLimitBegin) {
+        this.goHomeLimitBegin = goHomeLimitBegin;
+    }
 
-	@Column(name = "go_home_limit_end", nullable = false, length = 3)
-	public Integer getGoHomeLimitEnd() {
-		return goHomeLimitEnd;
-	}
+    @Column(name = "go_home_limit_end", nullable = false, length = 3)
+    public Integer getGoHomeLimitEnd() {
+        return goHomeLimitEnd;
+    }
 
-	public void setGoHomeLimitEnd(Integer goHomeLimitEnd) {
-		this.goHomeLimitEnd = goHomeLimitEnd;
-	}
+    public void setGoHomeLimitEnd(Integer goHomeLimitEnd) {
+        this.goHomeLimitEnd = goHomeLimitEnd;
+    }
 
-	@Column(name = "is_penalty_arrive_late", nullable = false)
-	public Boolean getIsPenaltyArriveLate() {
-		return isPenaltyArriveLate;
-	}
+    @Column(name = "is_penalty_arrive_late", nullable = false)
+    public Boolean getIsPenaltyArriveLate() {
+        return isPenaltyArriveLate;
+    }
 
-	public void setIsPenaltyArriveLate(Boolean isPenaltyArriveLate) {
-		this.isPenaltyArriveLate = isPenaltyArriveLate;
-	}
+    public void setIsPenaltyArriveLate(Boolean isPenaltyArriveLate) {
+        this.isPenaltyArriveLate = isPenaltyArriveLate;
+    }
 
-	@Column(name = "is_penalty_go_home_early", nullable = false)
-	public Boolean getIsPenaltyGoHomeEarly() {
-		return isPenaltyGoHomeEarly;
-	}
+    @Column(name = "is_penalty_go_home_early", nullable = false)
+    public Boolean getIsPenaltyGoHomeEarly() {
+        return isPenaltyGoHomeEarly;
+    }
 
-	public void setIsPenaltyGoHomeEarly(Boolean isPenaltyGoHomeEarly) {
-		this.isPenaltyGoHomeEarly = isPenaltyGoHomeEarly;
-	}
+    public void setIsPenaltyGoHomeEarly(Boolean isPenaltyGoHomeEarly) {
+        this.isPenaltyGoHomeEarly = isPenaltyGoHomeEarly;
+    }
 
-	@Column(name = "is_manage_break_time", nullable = false)
-	public Boolean getIsManageBreakTime() {
-		return isManageBreakTime;
-	}
+    @Column(name = "is_manage_break_time", nullable = false)
+    public Boolean getIsManageBreakTime() {
+        return isManageBreakTime;
+    }
 
-	public void setIsManageBreakTime(Boolean isManageBreakTime) {
-		this.isManageBreakTime = isManageBreakTime;
-	}
+    public void setIsManageBreakTime(Boolean isManageBreakTime) {
+        this.isManageBreakTime = isManageBreakTime;
+    }
 
-	@Temporal(TemporalType.TIME)
-	@Column(name = "break_hour_begin", length = 3)
-	public Date getBreakHourBegin() {
-		return breakHourBegin;
-	}
+    @Temporal(TemporalType.TIME)
+    @Column(name = "break_hour_begin", length = 3)
+    public Date getBreakHourBegin() {
+        return breakHourBegin;
+    }
 
-	public void setBreakHourBegin(Date breakHourBegin) {
-		this.breakHourBegin = breakHourBegin;
-	}
+    public void setBreakHourBegin(Date breakHourBegin) {
+        this.breakHourBegin = breakHourBegin;
+    }
 
-	@Temporal(TemporalType.TIME)
-	@Column(name = "break_hour_end", length = 3)
-	public Date getBreakHourEnd() {
-		return breakHourEnd;
-	}
+    @Temporal(TemporalType.TIME)
+    @Column(name = "break_hour_end", length = 3)
+    public Date getBreakHourEnd() {
+        return breakHourEnd;
+    }
 
-	public void setBreakHourEnd(Date breakHourEnd) {
-		this.breakHourEnd = breakHourEnd;
-	}
+    public void setBreakHourEnd(Date breakHourEnd) {
+        this.breakHourEnd = breakHourEnd;
+    }
 
-	@Column(name = "break_start_limit_begin", length = 3)
-	public Integer getBreakStartLimitBegin() {
-		return breakStartLimitBegin;
-	}
+    @Column(name = "break_start_limit_begin", length = 3)
+    public Integer getBreakStartLimitBegin() {
+        return breakStartLimitBegin;
+    }
 
-	public void setBreakStartLimitBegin(Integer breakStartLimitBegin) {
-		this.breakStartLimitBegin = breakStartLimitBegin;
-	}
+    public void setBreakStartLimitBegin(Integer breakStartLimitBegin) {
+        this.breakStartLimitBegin = breakStartLimitBegin;
+    }
 
-	@Column(name = "break_start_limit_end", length = 3)
-	public Integer getBreakStartLimitEnd() {
-		return breakStartLimitEnd;
-	}
+    @Column(name = "break_start_limit_end", length = 3)
+    public Integer getBreakStartLimitEnd() {
+        return breakStartLimitEnd;
+    }
 
-	public void setBreakStartLimitEnd(Integer breakStartLimitEnd) {
-		this.breakStartLimitEnd = breakStartLimitEnd;
-	}
+    public void setBreakStartLimitEnd(Integer breakStartLimitEnd) {
+        this.breakStartLimitEnd = breakStartLimitEnd;
+    }
 
-	@Column(name = "break_finish_limit_begin", length = 3)
-	public Integer getBreakFinishLimitBegin() {
-		return breakFinishLimitBegin;
-	}
+    @Column(name = "break_finish_limit_begin", length = 3)
+    public Integer getBreakFinishLimitBegin() {
+        return breakFinishLimitBegin;
+    }
 
-	public void setBreakFinishLimitBegin(Integer breakFinishLimitBegin) {
-		this.breakFinishLimitBegin = breakFinishLimitBegin;
-	}
+    public void setBreakFinishLimitBegin(Integer breakFinishLimitBegin) {
+        this.breakFinishLimitBegin = breakFinishLimitBegin;
+    }
 
-	@Column(name = "break_finish_limit_end", length = 3)
-	public Integer getBreakFinishLimitEnd() {
-		return breakFinishLimitEnd;
-	}
+    @Column(name = "break_finish_limit_end", length = 3)
+    public Integer getBreakFinishLimitEnd() {
+        return breakFinishLimitEnd;
+    }
 
-	public void setBreakFinishLimitEnd(Integer breakFinishLimitEnd) {
-		this.breakFinishLimitEnd = breakFinishLimitEnd;
-	}
+    public void setBreakFinishLimitEnd(Integer breakFinishLimitEnd) {
+        this.breakFinishLimitEnd = breakFinishLimitEnd;
+    }
 
-	@Column(name = "is_penalty_break_start_early")
-	public Boolean getIsPenaltyBreakStartEarly() {
-		return isPenaltyBreakStartEarly;
-	}
+    @Column(name = "is_penalty_break_start_early")
+    public Boolean getIsPenaltyBreakStartEarly() {
+        return isPenaltyBreakStartEarly;
+    }
 
-	public void setIsPenaltyBreakStartEarly(Boolean isPenaltyBreakStartEarly) {
-		this.isPenaltyBreakStartEarly = isPenaltyBreakStartEarly;
-	}
+    public void setIsPenaltyBreakStartEarly(Boolean isPenaltyBreakStartEarly) {
+        this.isPenaltyBreakStartEarly = isPenaltyBreakStartEarly;
+    }
 
-	@Column(name = "is_penalty_break_finish_late")
-	public Boolean getIsPenaltyBreakFinishLate() {
-		return isPenaltyBreakFinishLate;
-	}
+    @Column(name = "is_penalty_break_finish_late")
+    public Boolean getIsPenaltyBreakFinishLate() {
+        return isPenaltyBreakFinishLate;
+    }
 
-	public void setIsPenaltyBreakFinishLate(Boolean isPenaltyBreakFinishLate) {
-		this.isPenaltyBreakFinishLate = isPenaltyBreakFinishLate;
-	}
+    public void setIsPenaltyBreakFinishLate(Boolean isPenaltyBreakFinishLate) {
+        this.isPenaltyBreakFinishLate = isPenaltyBreakFinishLate;
+    }
 
-	@Column(name = "created_by", length = 45)
+    @Column(name = "created_by", length = 45)
     public String getCreatedBy() {
         return this.createdBy;
     }
@@ -304,6 +308,15 @@ public class WtWorkingHour implements java.io.Serializable {
 
     public void setUpdatedOn(Date updatedOn) {
         this.updatedOn = updatedOn;
+    }
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "wtWorkingHour")
+    public Set<WtScheduleShift> getWtScheduleShifts() {
+        return this.wtScheduleShifts;
+    }
+
+    public void setWtScheduleShifts(Set<WtScheduleShift> wtScheduleShifts) {
+        this.wtScheduleShifts = wtScheduleShifts;
     }
 
 }
