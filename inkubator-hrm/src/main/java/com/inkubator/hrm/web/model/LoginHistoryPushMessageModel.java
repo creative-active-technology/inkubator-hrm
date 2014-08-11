681df8d82ea20da84db817f0ab20ec2149d1d702
rizkykojek <rizkykojek@gmail.com>
2014-08-11 13:52:17 +0700

    Login History: - update table in dashboard - push message
    Access History:
    - update table in dashboard

---
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/web/model/LoginHistoryPushMessageModel.java
@@ -0,0 +1,50 @@
+package com.inkubator.hrm.web.model;
+/**
+ *
+ * @author rizkykojek
+ */
+public class LoginHistoryPushMessageModel {
+	private String loginDate;
+	private String loginTime;
+	private String loginName;
+	private String growlMessage;
+	private String growlTitle;
+	private Boolean isLogin;
+	
+	public String getLoginDate() {
+		return loginDate;
+	}
+	public void setLoginDate(String loginDate) {
+		this.loginDate = loginDate;
+	}
+	public String getLoginTime() {
+		return loginTime;
+	}
+	public void setLoginTime(String loginTime) {
+		this.loginTime = loginTime;
+	}
+	public String getLoginName() {
+		return loginName;
+	}
+	public void setLoginName(String loginName) {
+		this.loginName = loginName;
+	}
+	public String getGrowlMessage() {
+		return growlMessage;
+	}
+	public void setGrowlMessage(String growlMessage) {
+		this.growlMessage = growlMessage;
+	}
+	public String getGrowlTitle() {
+		return growlTitle;
+	}
+	public void setGrowlTitle(String growlTitle) {
+		this.growlTitle = growlTitle;
+	}
+	public Boolean getIsLogin() {
+		return isLogin;
+	}
+	public void setIsLogin(Boolean isLogin) {
+		this.isLogin = isLogin;
+	}
+}
