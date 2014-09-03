e92a4594c83075902d1b1225babfee5861241fcd
rizkykojek <rizkykojek@gmail.com>
2014-09-03 13:46:20 +0700

    approval process

--- inkubator-hrm/src/main/java/com/inkubator/hrm/service/impl/ApprovalActivityServiceImpl.java
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/service/impl/ApprovalActivityServiceImpl.java
@@ -1,16 +1,31 @@
 package com.inkubator.hrm.service.impl;
 
+import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.hibernate.criterion.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.inkubator.common.util.RandomNumberUtil;
 import com.inkubator.datacore.service.impl.IServiceImpl;
+import com.inkubator.hrm.HRMConstant;
 import com.inkubator.hrm.dao.ApprovalActivityDao;
+import com.inkubator.hrm.dao.ApprovalDefinitionDao;
+import com.inkubator.hrm.dao.EmpDataDao;
+import com.inkubator.hrm.dao.HrmUserDao;
 import com.inkubator.hrm.entity.ApprovalActivity;
+import com.inkubator.hrm.entity.ApprovalDefinition;
+import com.inkubator.hrm.entity.EmpData;
+import com.inkubator.hrm.entity.HrmUser;
+import com.inkubator.hrm.entity.Jabatan;
 import com.inkubator.hrm.service.ApprovalActivityService;
+import com.inkubator.securitycore.util.UserInfoUtil;
 
 /**
  *
@@ -22,6 +37,12 @@ public class ApprovalActivityServiceImpl extends IServiceImpl implements Approva
 
 	@Autowired
 	private ApprovalActivityDao approvalActivityDao;
+	@Autowired
+	private ApprovalDefinitionDao approvalDefinitionDao;
+	@Autowired
+	private EmpDataDao empDataDao;
+	@Autowired
+	private HrmUserDao hrmUserDao;
 	
 	@Override
 	public ApprovalActivity getEntiyByPK(String id) throws Exception {
@@ -42,9 +63,10 @@ public class ApprovalActivityServiceImpl extends IServiceImpl implements Approva
 	}
 
 	@Override
+	@Transactional(readOnly = false, isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
 	public void save(ApprovalActivity entity) throws Exception {
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose ECLIPSE Preferences | Code Style | Code Templates.
-
+		entity.setId(Long.parseLong(RandomNumberUtil.getRandomNumber(9)));
+		approvalActivityDao.save(entity);
 	}
 
 	@Override
@@ -228,6 +250,162 @@ public class ApprovalActivityServiceImpl extends IServiceImpl implements Approva
 			int maxResults, Order order, Byte isActive) throws Exception {
 		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose ECLIPSE Preferences | Code Style | Code Templates.
 
+	}
+
+	@Override
+	@Transactional(readOnly = true, isolation = Isolation.REPEATABLE_READ, propagation = Propagation.SUPPORTS, timeout = 50)
+	public ApprovalActivity checkApprovalProcess(String processName) throws Exception {
+		
+		ApprovalActivity appActivity = null;
+		List<ApprovalDefinition> listAppDef = approvalDefinitionDao.getAllDataByNameAndProcessType(processName, HRMConstant.APPROVAL_PROCESS, Order.asc("sequence"));
+		if(!listAppDef.isEmpty()){ //if not empty
+			ApprovalDefinition appDef = listAppDef.get(0);
+			String approverUserId = this.getApproverByAppDefinition(appDef);
+			
+			if(StringUtils.isNotEmpty(approverUserId)){				
+				appActivity = new ApprovalActivity();				
+				appActivity.setApprovalDefinition(appDef);			
+				appActivity.setApprovedBy(approverUserId);
+				appActivity.setApprovalStatus(HRMConstant.APPROVAL_STATUS_WAITING);
+				appActivity.setSequence(1);
+				appActivity.setApprovalCount(0);
+				appActivity.setRejectCount(0);
+				appActivity.setActivityNumber(RandomNumberUtil.getRandomNumber(9));
+				appActivity.setNotificationSend(false);
+				appActivity.setRequestBy(UserInfoUtil.getUserName());
+				
+			}
+		}
+		
+		return appActivity;
+	}
+	
+	private String getApproverByAppDefinition(ApprovalDefinition appDef){
+		String userId = StringUtils.EMPTY;
+		
+		if(StringUtils.equals(appDef.getApproverType(), HRMConstant.APPROVAL_TYPE_INDIVIDUAL)){  //approver based on individual
+			HrmUser approver = appDef.getHrmUserByApproverIndividual();
+			userId = approver.getUserId();
+			
+		} else if(StringUtils.equals(appDef.getApproverType(), HRMConstant.APPROVAL_TYPE_POSITION)){ //approver based on position			
+			Jabatan jabatan = appDef.getJabatanByApproverPosition();			
+			userId = this.getApproverByJabatanId(jabatan.getId());
+			
+		} else if(StringUtils.equals(appDef.getApproverType(), HRMConstant.APPROVAL_TYPE_DEPARTMENT)){ ////approver based on department
+			
+			
+		}
+		
+		return userId;
+	}
+	
+	private String getApproverByJabatanId(long jabatanId){
+		String userId = StringUtils.EMPTY;
+		
+		/** jika dalam satu jabatan yg sama terdapat beberapa employee, maka pilih employee berdasarkan joinDate/TMB yg terlama 
+		 *  itulah kenapa di order desc "joinDate" */
+		List<EmpData> employees = empDataDao.getAllDataByJabatanId(jabatanId, Order.desc("joinDate"));		
+		if(!employees.isEmpty()) { //if not empty
+			EmpData empData = employees.get(0);				
+			if(!empData.getHrmUsers().isEmpty()){ //if not empty
+				HrmUser approver = empData.getHrmUsers().iterator().next();
+				userId = approver.getUserId();					
+			}
+		}
+		
+		return userId;
+	}
+	
+	@Override
+	@Transactional(readOnly = false, isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+	public ApprovalActivity approved(Long appActivityId, String comment) throws Exception {
+		
+		/* update APPROVED approval activity */
+		ApprovalActivity approvalActivity = approvalActivityDao.getEntiyByPK(appActivityId);
+		approvalActivity.setApprovalStatus(HRMConstant.APPROVAL_STATUS_APPROVED);
+		approvalActivity.setApprovalCommment(comment);
+		int approvedCount = approvalActivity.getApprovalCount() + 1; //increment +1
+		approvalActivity.setApprovalCount(approvedCount);
+		approvalActivity.setApprovalTime(new Date());
+		approvalActivityDao.update(approvalActivity);
+		
+		
+		/* create new approval activity (if any)*/
+		ApprovalActivity newEntity = null;
+		ApprovalDefinition previousAppDef = approvalActivity.getApprovalDefinition();
+		
+		/** cek apakah approver count sudah memenuhi dari minimal approver di approval definition 
+		 *  1. jika belum memenuhi, maka lanjut ke checking atasannya untuk proses approval-nya
+		 *  2. jika sudah memenuhi maka lanjut ke proses checking approval definition selanjutnya */
+		if(approvalActivity.getApprovalCount() < previousAppDef.getMinApprover()){
+			
+			//proses no. 1
+			HrmUser user = hrmUserDao.getByUserId(approvalActivity.getApprovedBy());
+			Jabatan jabatan = user.getEmpData().getJabatanByJabatanId();
+			Jabatan parentJabatan = jabatan.getJabatan();
+			/** jika approver mempunyai atasan maka lanjut approval ke atasannya, 
+			 *  jika tidak punya atasan langsung approve saja(dilewat proses checking)*/
+			if(parentJabatan!=null){				
+				String approverUserId = this.getApproverByJabatanId(parentJabatan.getId());
+				newEntity = this.createNewApprovalActivity(approverUserId, previousAppDef, approvalActivity);
+				approvalActivityDao.save(newEntity);
+			}
+			
+		} else {
+			
+			//proses no. 2
+			List<ApprovalDefinition> listAppDef = approvalDefinitionDao.getAllDataByNameAndProcessTypeAndSequenceGreater(previousAppDef.getName(), previousAppDef.getProcessType(), previousAppDef.getSequence());
+			if(listAppDef.size() > 0){
+				ApprovalDefinition appDef = listAppDef.get(0);
+				String approverUserId = this.getApproverByAppDefinition(appDef);
+				newEntity = this.createNewApprovalActivity(approverUserId, appDef, approvalActivity);
+				approvalActivityDao.save(newEntity);
+			}
+		}
+		
+		
+		if(newEntity != null){
+			// jika newEntity sama dengan null, berarti sudah tidak ada lagi proses approval, lanjut ke saving objek dari "pendingData" json
+			return newEntity;
+		} else {
+			// jika newEntity tidak sama dengan null, maka lanjut ke proses kirim email ke approver
+			return approvalActivity;
+		}
+	}
+	
+	private ApprovalActivity createNewApprovalActivity(String approverUserId, ApprovalDefinition appDef, ApprovalActivity previousAppActv){
+		ApprovalActivity newEntity = new ApprovalActivity();
+		newEntity.setId(Long.parseLong(RandomNumberUtil.getRandomNumber(9)));
+		newEntity.setApprovalDefinition(appDef);			
+		newEntity.setApprovedBy(approverUserId);
+		newEntity.setApprovalStatus(HRMConstant.APPROVAL_STATUS_WAITING);
+		newEntity.setNotificationSend(false);
+		
+		//copy value from previous approval activity
+		newEntity.setSequence(previousAppActv.getSequence() + 1); //increment +1
+		newEntity.setApprovalCount(previousAppActv.getApprovalCount());
+		newEntity.setRejectCount(previousAppActv.getRejectCount());
+		newEntity.setActivityNumber(previousAppActv.getActivityNumber());			
+		newEntity.setRequestBy(previousAppActv.getRequestBy());
+		newEntity.setPendingData(previousAppActv.getPendingData());		
+		
+		return newEntity;
+	}
+	
+	@Override
+	@Transactional(readOnly = false, isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+	public ApprovalActivity rejected(Long appActivityId, String comment) throws Exception {
+		
+		/* update REJECTED approval activity */
+		ApprovalActivity approvalActivity = approvalActivityDao.getEntiyByPK(appActivityId);
+		approvalActivity.setApprovalStatus(HRMConstant.APPROVAL_STATUS_REJECTED);
+		approvalActivity.setApprovalCommment(comment);
+		int rejectedCount = approvalActivity.getRejectCount() + 1; //increment +1
+		approvalActivity.setRejectCount(rejectedCount);
+		approvalActivity.setApprovalTime(new Date());
+		approvalActivityDao.save(approvalActivity);
+		
+		return null;
 	}
 
 }
