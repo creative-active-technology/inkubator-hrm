959da09ecb04222a952e03d95be395d979fd35fe
Deni Husni FR <Deni Husni FR@Positron>
2014-09-04 10:11:45 +0700

    Fix user individual on approval and ondelay condition

--- inkubator-hrm/src/main/java/com/inkubator/hrm/web/HomeApproalActivityController.java
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/web/HomeApproalActivityController.java
@@ -5,8 +5,14 @@
  */
 package com.inkubator.hrm.web;
 
+import com.inkubator.hrm.entity.ApprovalActivity;
+import com.inkubator.hrm.service.ApprovalActivityService;
 import com.inkubator.webcore.controller.BaseController;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 
 /**
@@ -17,5 +23,46 @@ import javax.faces.bean.RequestScoped;
 @RequestScoped
 public class HomeApproalActivityController extends BaseController {
 
+    @ManagedProperty(value = "#{approvalActivityService}")
+    private ApprovalActivityService approvalActivityService;
+    private List<ApprovalActivity> requestHistory = new ArrayList<>();
+    private List<ApprovalActivity> pendingTask = new ArrayList<>();
+    private List<ApprovalActivity> pendingRequest = new ArrayList<>();
+
+    @PostConstruct
+    @Override
+    public void initialization() {
+
+    }
+
+    public List<ApprovalActivity> getRequestHistory() {
+        return requestHistory;
+    }
+
+    public void setRequestHistory(List<ApprovalActivity> requestHistory) {
+        this.requestHistory = requestHistory;
+    }
+
+    public List<ApprovalActivity> getPendingTask() {
+        return pendingTask;
+    }
+
+    public void setPendingTask(List<ApprovalActivity> pendingTask) {
+        this.pendingTask = pendingTask;
+    }
+
+    public List<ApprovalActivity> getPendingRequest() {
+        return pendingRequest;
+    }
+
+    public void setPendingRequest(List<ApprovalActivity> pendingRequest) {
+        this.pendingRequest = pendingRequest;
+    }
+
+    public void setApprovalActivityService(ApprovalActivityService approvalActivityService) {
+        this.approvalActivityService = approvalActivityService;
+    }
+    
+    
     
 }
