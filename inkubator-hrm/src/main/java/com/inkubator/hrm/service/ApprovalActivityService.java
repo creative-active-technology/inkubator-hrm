8ef5b73c347d8d99e0111c24e2c45a4fbb6a36e7
Deni <Deni@Lenovo-PC>
2014-09-08 16:57:36 +0700

    loan schema

--- inkubator-hrm/src/main/java/com/inkubator/hrm/service/ApprovalActivityService.java
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/service/ApprovalActivityService.java
@@ -22,10 +22,11 @@ public interface ApprovalActivityService extends IService<ApprovalActivity> {
     
     public ApprovalActivity getEntityByPkWithAllRelation(Long id) throws Exception;
 
+    public List<ApprovalActivity> getRequestHistory(String userName) throws Exception;
+
     public List<ApprovalActivity> getReguestHistoryById(long id) throws Exception;
 
     public List<ApprovalActivity> getPendingRequest(String userName) throws Exception;
 
     public List<ApprovalActivity> getPendingTask(String userName) throws Exception;
-
 }
