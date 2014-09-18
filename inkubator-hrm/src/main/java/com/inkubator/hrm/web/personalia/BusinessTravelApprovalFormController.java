26de4f1c2d8d0af38b1a8aef40ef059a5fd4e1e4
rizkykojek <rizkykojek@gmail.com>
2014-09-18 12:37:47 +0700

    refactor package businestravel controller, move to personalia

--- inkubator-hrm/src/main/java/com/inkubator/hrm/web/personalia/BusinessTravelApprovalFormController.java
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/web/personalia/BusinessTravelApprovalFormController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.inkubator.hrm.web.workingtime;
+package com.inkubator.hrm.web.personalia;
 
 import java.util.List;
 
