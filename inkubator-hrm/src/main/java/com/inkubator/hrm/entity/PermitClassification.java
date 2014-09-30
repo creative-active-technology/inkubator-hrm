c11f56c40dcc6b6d3fade2fcf5c4310fc8c7d39e
Deni Husni FR <Deni Husni FR@Positron>
2014-09-30 10:25:20 +0700

    Fix and Migrasi  jsf 2.1.29 and primefaces 4.0.22

--- inkubator-hrm/src/main/java/com/inkubator/hrm/entity/PermitClassification.java
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/entity/PermitClassification.java
@@ -242,7 +242,7 @@ public class PermitClassification  implements java.io.Serializable {
     }
 
     
-    @Column(name="description", length=65535)
+     @Column(name = "description", length = 65535, columnDefinition = "Text")
     public String getDescription() {
         return this.description;
     }
