bc603efd79f438b67e537b57f603dddbdeea8668
User <User@Lenovo-PC>
2014-12-02 14:43:01 +0700

    Add Romanov Util

--- inkubator-hrm/src/main/java/com/inkubator/hrm/util/TestAh.java
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/util/TestAh.java
@@ -5,9 +5,6 @@
  */
 package com.inkubator.hrm.util;
 
-import java.util.Date;
-import java.util.List;
-
 /**
  *
  * @author Deni Husni FR
@@ -24,34 +21,35 @@ public class TestAh {
 //        
 //        System.out.println(HRMFinanceLib.interestRatePaymentFlateMode(24000000,  5));
 //        
-//        
-//        System.out.println(HRMFinanceLib.anuitas(50000000, 24, 24,6));
-        LoanPayment loanPayment = new LoanPayment();
-        loanPayment.setBungaPertahun(33.07);
-        loanPayment.setLamaPinjaman(23);
-        loanPayment.setPaymentPeriod(1);
-        loanPayment.setTotalPinjaman(8687004.79);
-        loanPayment.setTanggalBayar(new Date());
-//        HRMFinanceLib.getLoanPaymentFlateMode(loanPayment);
-//        
-//        System.out.println(HRMFinanceLib.getLoanPaymentFlateMode(loanPayment).getJadwalPembayarans().get(0).getPokok());
-//         System.out.println(HRMFinanceLib.getLoanPaymentFlateMode(loanPayment).getJadwalPembayarans().get(0).getBunga());
-//        loanPayment = HRMFinanceLib.getLoanPaymentFlateMode(loanPayment);
-////        System.out.println(" hahahah");
-//        List<JadwalPembayaran> jadwalPembayarans = loanPayment.getJadwalPembayarans();
-//        for (JadwalPembayaran jadwalPembayaran : jadwalPembayarans) {
-//            System.out.println(jadwalPembayaran.getSisaUtang());
+////        
+////        System.out.println(HRMFinanceLib.anuitas(50000000, 24, 24,6));
+//        LoanPayment loanPayment = new LoanPayment();
+//        loanPayment.setBungaPertahun(33.07);
+//        loanPayment.setLamaPinjaman(23);
+//        loanPayment.setPaymentPeriod(1);
+//        loanPayment.setTotalPinjaman(8687004.79);
+//        loanPayment.setTanggalBayar(new Date());
+////        HRMFinanceLib.getLoanPaymentFlateMode(loanPayment);
+////        
+////        System.out.println(HRMFinanceLib.getLoanPaymentFlateMode(loanPayment).getJadwalPembayarans().get(0).getPokok());
+////         System.out.println(HRMFinanceLib.getLoanPaymentFlateMode(loanPayment).getJadwalPembayarans().get(0).getBunga());
+////        loanPayment = HRMFinanceLib.getLoanPaymentFlateMode(loanPayment);
+//////        System.out.println(" hahahah");
+////        List<JadwalPembayaran> jadwalPembayarans = loanPayment.getJadwalPembayarans();
+////        for (JadwalPembayaran jadwalPembayaran : jadwalPembayarans) {
+////            System.out.println(jadwalPembayaran.getSisaUtang());
+////            System.out.println(jadwalPembayaran);
+////        }
+//
+//        loanPayment = HRMFinanceLib.getLoanPaymentAnuitas(loanPayment);
+//        System.out.println(" hahahah");
+//        List<JadwalPembayaran> jadwalPembayaransdd = loanPayment.getJadwalPembayarans();
+//        for (JadwalPembayaran jadwalPembayaran : jadwalPembayaransdd) {
+//            System.out.println(jadwalPembayaran.getAngsuran());
 //            System.out.println(jadwalPembayaran);
 //        }
 
-        loanPayment = HRMFinanceLib.getLoanPaymentAnuitas(loanPayment);
-        System.out.println(" hahahah");
-        List<JadwalPembayaran> jadwalPembayaransdd = loanPayment.getJadwalPembayarans();
-        for (JadwalPembayaran jadwalPembayaran : jadwalPembayaransdd) {
-            System.out.println(jadwalPembayaran.getAngsuran());
-            System.out.println(jadwalPembayaran);
-        }
-
+        System.out.println(RomanovUtil.convertToRoman(12));
     }
 
 }
