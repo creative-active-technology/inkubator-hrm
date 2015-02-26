f504bea5922b246d87869d5f393ebebe36361a43
EKA <EKA@Positron>
2015-02-26 13:56:15 +0700

    Feature #692 : Bank Grup CRUD

---
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/web/model/BankGroupModel.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.inkubator.hrm.web.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author EKA
+ */
+public class BankGroupModel implements Serializable{
+    private Long id;
+    private String code;
+    private String name;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
