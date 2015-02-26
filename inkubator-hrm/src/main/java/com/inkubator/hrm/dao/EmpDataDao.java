1965324dade06e67db4a58c529185810308cd22b
dicky <dicky@inkuba-laptop>
2015-02-25 23:46:03 -0800

    Commit Task #656_Laporan_Persiapan_Pensiun

--- inkubator-hrm/src/main/java/com/inkubator/hrm/dao/EmpDataDao.java
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/dao/EmpDataDao.java
@@ -12,6 +12,7 @@ import com.inkubator.hrm.web.model.DistributionLeaveSchemeModel;
 import com.inkubator.hrm.web.model.DistributionOvetTimeModel;
 import com.inkubator.hrm.web.model.PermitDistributionModel;
 import com.inkubator.hrm.web.model.PlacementOfEmployeeWorkScheduleModel;
+import com.inkubator.hrm.web.model.ReportEmpPensionPreparationModel;
 import com.inkubator.hrm.web.model.ReportEmployeeEducationViewModel;
 import com.inkubator.hrm.web.model.WtFingerExceptionModel;
 import com.inkubator.hrm.web.search.EmpDataSearchParameter;
@@ -125,4 +126,8 @@ public interface EmpDataDao extends IDAO<EmpData> {
     public List<EmpData> getReportRekapJabatanByParam(List<Long> listDepartmentId, List<Long> listEmpTypeId, int firstResult, int maxResults, Order order);
 
     public Long getTotalReportRekapJabatanByParam(List<Long> listDepartmentId, List<Long> listEmpTypeId);
+    
+    public List<ReportEmpPensionPreparationModel> getReportPensionPreparementByParam(List<Long> listDepartmentId, List<Long> listEmpTypeId, List<Integer> listEmpAges, int firstResult, int maxResults, Order order);
+
+    public Long getTotalReportPensionPreparementByParam(List<Long> listDepartmentId, List<Long> listEmpTypeId, List<Integer> listEmpAges);
 }
