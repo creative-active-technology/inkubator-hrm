aed6c6d1959eb07c25a53ee2f6a0ca25983395e1
rizkykojek <rizkykojek@gmail.com>
2015-03-20 11:31:07 +0700

    Feature #727 "Reimburst versi baru: Type, Skema, Skema karyawan" … - adding reimbursement type configuration

---
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/dao/impl/RmbsSchemaListOfTypeDaoImpl.java
@@ -0,0 +1,48 @@
+package com.inkubator.hrm.dao.impl;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.FetchMode;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Repository;
+
+import com.inkubator.datacore.dao.impl.IDAOImpl;
+import com.inkubator.hrm.dao.RmbsSchemaListOfTypeDao;
+import com.inkubator.hrm.entity.RmbsSchemaListOfType;
+import com.inkubator.hrm.entity.RmbsSchemaListOfTypeId;
+
+/**
+ *
+ * @author rizkykojek
+ */
+@Repository(value = "rmbsSchemaListOfTypeDao")
+@Lazy
+public class RmbsSchemaListOfTypeDaoImpl extends IDAOImpl<RmbsSchemaListOfType> implements RmbsSchemaListOfTypeDao {
+
+	@Override
+	public Class<RmbsSchemaListOfType> getEntityClass() {
+		return RmbsSchemaListOfType.class;
+		
+	}
+	
+	@Override
+	public List<RmbsSchemaListOfType> getAllDataByRmbsSchemaId(Long rmbsSchemaId) {
+		Criteria criteria = getCurrentSession().createCriteria(getEntityClass());
+		criteria.add(Restrictions.eq("rmbsSchema.id", rmbsSchemaId));
+		criteria.setFetchMode("rmbsType", FetchMode.JOIN);
+		return criteria.list();
+	}
+
+	@Override
+	public RmbsSchemaListOfType getEntityByPk(RmbsSchemaListOfTypeId id) {
+		Criteria criteria = getCurrentSession().createCriteria(getEntityClass());
+		criteria.add(Restrictions.eq("id.rmbsSchemaId", id.getRmbsSchemaId()));
+		criteria.add(Restrictions.eq("id.rmbsTypeId", id.getRmbsTypeId()));
+		
+		return (RmbsSchemaListOfType) criteria.uniqueResult();
+		
+	}
+	
+}
