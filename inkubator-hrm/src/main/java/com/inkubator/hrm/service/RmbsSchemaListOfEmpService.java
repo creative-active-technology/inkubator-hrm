ff272beead0f0f2b8774399233148d8c68282c92
rizkykojek <rizkykojek@gmail.com>
2015-03-24 13:14:13 +0700

    Feature #727 "Reimburst versi baru: Type, Skema, Skema karyawan" - employee reimbursement schema view

---
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/service/RmbsSchemaListOfEmpService.java
@@ -0,0 +1,22 @@
+package com.inkubator.hrm.service;
+
+import java.util.List;
+
+import org.hibernate.criterion.Order;
+
+import com.inkubator.datacore.service.IService;
+import com.inkubator.hrm.entity.RmbsSchemaListOfEmp;
+import com.inkubator.hrm.web.model.RmbsSchemaEmpViewModel;
+import com.inkubator.hrm.web.search.RmbsSchemaEmpSearchParameter;
+
+/**
+ *
+ * @author rizkykojek
+ */
+public interface RmbsSchemaListOfEmpService extends IService<RmbsSchemaListOfEmp> {
+
+	public List<RmbsSchemaEmpViewModel> getByParamEmployeeSchema(RmbsSchemaEmpSearchParameter parameter, int firstResult, int maxResults, Order orderable) throws Exception;
+	
+	public Long getTotalByParamEmployeeSchema(RmbsSchemaEmpSearchParameter parameter) throws Exception;
+	
+}
