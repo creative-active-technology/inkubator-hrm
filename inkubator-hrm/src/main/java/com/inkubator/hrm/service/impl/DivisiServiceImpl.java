4b6a4f3a3de3d9e0dfe83a92fbb824e41f9a7993
EKA <EKA@Positron>
2015-03-24 15:05:02 +0700

    Feature #749 : Divisi

---
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/service/impl/DivisiServiceImpl.java
@@ -0,0 +1,243 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.inkubator.hrm.service.impl;
+
+import com.inkubator.common.util.RandomNumberUtil;
+import com.inkubator.datacore.service.impl.IServiceImpl;
+import com.inkubator.exception.BussinessException;
+import com.inkubator.hrm.dao.DepartmentDao;
+import com.inkubator.hrm.dao.DivisiDao;
+import com.inkubator.hrm.entity.Divisi;
+import com.inkubator.hrm.service.DivisiService;
+import com.inkubator.hrm.web.search.DivisiSearchParameter;
+import com.inkubator.securitycore.util.UserInfoUtil;
+import java.util.Date;
+import java.util.List;
+import org.hibernate.criterion.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author EKA
+ */
+@Service(value = "divisiService")
+@Lazy
+public class DivisiServiceImpl extends IServiceImpl implements DivisiService{
+    
+    @Autowired 
+    private DivisiDao divisiDao;
+    
+    @Autowired
+    private DepartmentDao departmentDao;
+
+    @Override
+    @Transactional(readOnly = true, isolation = Isolation.REPEATABLE_READ, propagation = Propagation.SUPPORTS, timeout = 50)
+    public List<Divisi> getByParam(DivisiSearchParameter searchParameter, int firstResult, int maxResults, Order order) throws Exception {
+        return divisiDao.getByParam(searchParameter, firstResult, maxResults, order);
+    }
+
+    @Override
+    @Transactional(readOnly = true, isolation = Isolation.REPEATABLE_READ, propagation = Propagation.SUPPORTS, timeout = 50)
+    public Long getTotalDivisiByParam(DivisiSearchParameter searchParameter) throws Exception {
+        return divisiDao.getTotalDivisiByParam(searchParameter);
+    }
+
+    @Override
+    public Divisi getEntiyByPK(String string) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Divisi getEntiyByPK(Integer intgr) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    @Transactional(readOnly = true, isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+    public Divisi getEntiyByPK(Long id) throws Exception {
+        return divisiDao.getEntiyByPK(id);
+    }
+
+    @Override
+    @Transactional(readOnly = false, isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+    public void save(Divisi entity) throws Exception {
+        long totalDuplicates = divisiDao.getTotalByCode(entity.getCode());
+        if(totalDuplicates > 0){
+            throw new BussinessException("marital.error_duplicate_marital_code");
+        }
+        entity.setId(Long.parseLong(RandomNumberUtil.getRandomNumber(9)));
+        entity.setDepartment(departmentDao.getEntiyByPK(entity.getDepartment().getId()));
+        entity.setCreatedBy(UserInfoUtil.getUserName());
+        entity.setCreatedOn(new Date());
+        this.divisiDao.save(entity);
+        
+    }
+
+    @Override
+    public void update(Divisi entity) throws Exception {
+        long totalDuplicates = divisiDao.getTotalByCodeAndNotId(entity.getCode(), entity.getId());
+        if(totalDuplicates > 0){
+            throw new BussinessException("marital.error_duplicate_marital_code");
+        }
+        Divisi update = divisiDao.getEntiyByPK(entity.getId());
+        update.setCode(entity.getCode());
+        update.setName(entity.getName());
+        update.setDescription(entity.getDescription());
+        update.setDepartment(entity.getDepartment());
+        update.setUpdatedBy(UserInfoUtil.getUserName());
+        update.setUpdatedOn(new Date());
+        this.divisiDao.update(update);
+    }
+
+    @Override
+    public void saveOrUpdate(Divisi t) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Divisi saveData(Divisi t) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Divisi updateData(Divisi t) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Divisi saveOrUpdateData(Divisi t) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Divisi getEntityByPkIsActive(String string, Integer intgr) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Divisi getEntityByPkIsActive(String string, Byte b) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Divisi getEntityByPkIsActive(String string, Boolean bln) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Divisi getEntityByPkIsActive(Integer intgr, Integer intgr1) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Divisi getEntityByPkIsActive(Integer intgr, Byte b) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Divisi getEntityByPkIsActive(Integer intgr, Boolean bln) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Divisi getEntityByPkIsActive(Long l, Integer intgr) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Divisi getEntityByPkIsActive(Long l, Byte b) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Divisi getEntityByPkIsActive(Long l, Boolean bln) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    @Transactional(readOnly = false, isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+    public void delete(Divisi entity) throws Exception {
+        this.divisiDao.delete(entity);
+    }
+
+    @Override
+    public void softDelete(Divisi t) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Long getTotalData() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Long getTotalDataIsActive(Boolean bln) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Long getTotalDataIsActive(Integer intgr) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Long getTotalDataIsActive(Byte b) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    @Transactional(readOnly = true, isolation = Isolation.REPEATABLE_READ, propagation = Propagation.SUPPORTS, timeout = 50)
+    public List<Divisi> getAllData() throws Exception {
+        return divisiDao.getAllData();
+    }
+
+    @Override
+    public List<Divisi> getAllData(Boolean bln) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Divisi> getAllData(Integer intgr) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Divisi> getAllData(Byte b) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Divisi> getAllDataPageAble(int i, int i1, Order order) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Divisi> getAllDataPageAbleIsActive(int i, int i1, Order order, Boolean bln) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Divisi> getAllDataPageAbleIsActive(int i, int i1, Order order, Integer intgr) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Divisi> getAllDataPageAbleIsActive(int i, int i1, Order order, Byte b) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    @Transactional(readOnly = true, isolation = Isolation.REPEATABLE_READ, propagation = Propagation.SUPPORTS, timeout = 50)
+    public Divisi getDepartmentNameByDivisiId(Long id) {
+        return this.divisiDao.getDepartmentNameByDivisiId(id);
+    }
+
+}
