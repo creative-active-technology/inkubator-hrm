f54bd55c81c24396cc2b939298a0dba4e67d64e7
EKA <EKA@Positron>
2015-03-27 15:44:58 +0700

    Feature #770 : Tipe Spesifikasi Kerja

---
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/web/model/OrgTypeOfSpecModel.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.inkubator.hrm.web.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author EKA
+ */
+public class OrgTypeOfSpecModel implements Serializable{
+    private Long id;
+    private String code;
+    private String name;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    
+}
