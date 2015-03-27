658cc71ab62a3f9f2fb67a0144f1325abe40aaa7
rizkykojek <rizkykojek@gmail.com>
2015-03-27 15:12:41 +0700

    Feature #745 : Penggajuan penggantian - Saving process - validation

--- inkubator-hrm/src/main/java/com/inkubator/hrm/dao/impl/RmbsApplicationDaoImpl.java
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/dao/impl/RmbsApplicationDaoImpl.java
@@ -1,5 +1,9 @@
 package com.inkubator.hrm.dao.impl;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
+import org.hibernate.Query;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Repository;
 
@@ -18,6 +22,26 @@ public class RmbsApplicationDaoImpl extends IDAOImpl<RmbsApplication> implements
 	@Override
 	public Class<RmbsApplication> getEntityClass() {
 		return RmbsApplication.class;
+		
+	}
+
+	@Override
+	public BigDecimal getTotalNominalByEmpDataIdAndRmbsTypeIdAndDateBetween(Long empDataId, Long rmbsTypeId, Date startDate, Date endDate) {
+		StringBuffer selectQuery = new StringBuffer(
+    			"SELECT SUM(nominal) " +
+    			"FROM RmbsApplication " +
+    			"WHERE empData.id = :empDataId " +
+    			"AND rmbsType.id = :rmbsTypeId " +
+    			"AND applicationDate >= :startDate " +
+    			"AND applicationDate <= :endDate");
+		
+    	Query hbm = getCurrentSession().createQuery(selectQuery.toString())
+    			.setParameter("empDataId", empDataId)
+    			.setParameter("rmbsTypeId", rmbsTypeId)
+    			.setParameter("startDate", startDate)
+    			.setParameter("endDate", endDate);
+    	
+    	return new BigDecimal(hbm.uniqueResult().toString());
 		
 	}
 
