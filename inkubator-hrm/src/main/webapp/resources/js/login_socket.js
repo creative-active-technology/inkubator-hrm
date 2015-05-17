9822731e799ae3702ea808068f6fd0f127c103dd
deni.fahri <deni.fahri@Positron>
2015-05-18 06:15:50 +0700

    Fix sample Push Messages

--- inkubator-hrm/src/main/webapp/resources/js/login_socket.js
+++ inkubator-hrm/src/main/webapp/resources/js/login_socket.js
@@ -1,1 +1,5 @@
-function handleLogin(a){PF("socketNotificationGrowl").renderMessage({summary:a.growlTitle,detail:a.growlMessage,severity:"info"});if(a.isLogin){var c=a.loginName,d=a.loginDate;a=a.loginTime;for(var b=0;5>b;b++)if($("#loginHistoryName_"+b).length){var e=$("#loginHistoryName_"+b).html(),f=$("#loginHistoryDate_"+b).html(),g=$("#loginHistoryTime_"+b).html();$("#loginHistoryName_"+b).html(c);$("#loginHistoryDate_"+b).html(d);$("#loginHistoryTime_"+b).html(a);c=e;d=f;a=g}}};
+//function handleLogin(a){PF("socketNotificationGrowl").renderMessage({summary:a.growlTitle,detail:a.growlMessage,severity:"info"});if(a.isLogin){var c=a.loginName,d=a.loginDate;a=a.loginTime;for(var b=0;5>b;b++)if($("#loginHistoryName_"+b).length){var e=$("#loginHistoryName_"+b).html(),f=$("#loginHistoryDate_"+b).html(),g=$("#loginHistoryTime_"+b).html();$("#loginHistoryName_"+b).html(c);$("#loginHistoryDate_"+b).html(d);$("#loginHistoryTime_"+b).html(a);c=e;d=f;a=g}}};
+function handleLogin(facesmessage) {
+    facesmessage.severity = 'info';
+    PF('socketNotificationGrowl').show([facesmessage]);
+}
