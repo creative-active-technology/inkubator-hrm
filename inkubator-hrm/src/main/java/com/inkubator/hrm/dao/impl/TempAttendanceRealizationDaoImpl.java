dd1ef7f74a5df68b701d9bfd464608a3c02a8ddc
deni.fahri <deni.fahri@Positron>
2015-06-01 15:16:35 +0700

    Add Enntitu DAO, Servve , Lazly : temp_attendance_realization

--- inkubator-hrm/src/main/java/com/inkubator/hrm/dao/impl/TempAttendanceRealizationDaoImpl.java
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/dao/impl/TempAttendanceRealizationDaoImpl.java
@@ -1,4 +1,5 @@
 package com.inkubator.hrm.dao.impl;
+
 import com.inkubator.hrm.entity.TempAttendanceRealization;
 import com.inkubator.datacore.dao.impl.IDAOImpl;
 import java.util.List;
@@ -11,6 +12,8 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Repository;
 import com.inkubator.hrm.dao.TempAttendanceRealizationDao;
 import com.inkubator.hrm.web.search.TempAttendanceRealizationSearchParameter;
+import org.hibernate.sql.JoinType;
+
 /**
  *
  * @author WebGenX
@@ -18,33 +21,53 @@ import com.inkubator.hrm.web.search.TempAttendanceRealizationSearchParameter;
 @Repository(value = "tempAttendanceRealizationDao")
 @Lazy
 public class TempAttendanceRealizationDaoImpl extends IDAOImpl<TempAttendanceRealization> implements TempAttendanceRealizationDao {
-@Override
-public Class<TempAttendanceRealization> getEntityClass() {
-return TempAttendanceRealization.class;
-}
-@Override
-public List<TempAttendanceRealization> getByParam(TempAttendanceRealizationSearchParameter parameter, int firstResult, int maxResults, Order order){
-Criteria criteria = getCurrentSession().createCriteria(getEntityClass());
-doSearchTempAttendanceRealizationByParam(parameter, criteria);
-criteria.addOrder(order);
-criteria.setFirstResult(firstResult);
-criteria.setMaxResults(maxResults);
-return criteria.list();
-}
 
+    @Override
+    public Class<TempAttendanceRealization> getEntityClass() {
+        return TempAttendanceRealization.class;
+    }
 
-@Override
-public Long getTotalTempAttendanceRealizationByParam(TempAttendanceRealizationSearchParameter parameter){
-Criteria criteria = getCurrentSession().createCriteria(getEntityClass());
-doSearchTempAttendanceRealizationByParam(parameter, criteria);
-return (Long) criteria.setProjection(Projections.rowCount()).uniqueResult();
-}
+    @Override
+    public List<TempAttendanceRealization> getByParam(TempAttendanceRealizationSearchParameter parameter, int firstResult, int maxResults, Order order) {
+        Criteria criteria = getCurrentSession().createCriteria(getEntityClass());
+        doSearchTempAttendanceRealizationByParam(parameter, criteria);
+        criteria.addOrder(order);
+        criteria.setFirstResult(firstResult);
+        criteria.setMaxResults(maxResults);
+        return criteria.list();
+    }
 
+    @Override
+    public Long getTotalTempAttendanceRealizationByParam(TempAttendanceRealizationSearchParameter parameter) {
+        Criteria criteria = getCurrentSession().createCriteria(getEntityClass());
+        doSearchTempAttendanceRealizationByParam(parameter, criteria);
+        return (Long) criteria.setProjection(Projections.rowCount()).uniqueResult();
+    }
 
-private void doSearchTempAttendanceRealizationByParam(TempAttendanceRealizationSearchParameter parameter, Criteria criteria){
-if (parameter.getCreatedBy() != null){
-criteria.add(Restrictions.like("createdBy", parameter.getCreatedBy(), MatchMode.ANYWHERE));
-}
-criteria.add(Restrictions.isNotNull("id"));
-}
+    private void doSearchTempAttendanceRealizationByParam(TempAttendanceRealizationSearchParameter parameter, Criteria criteria) {
+        if (parameter.getJabatan() != null) {
+            criteria.createAlias("empData", "ce", JoinType.INNER_JOIN);
+            criteria.createAlias("ce.jabatanByJabatanId", "jb", JoinType.INNER_JOIN);
+            criteria.add(Restrictions.like("jb.name", parameter.getJabatan(), MatchMode.ANYWHERE));
+        }
+
+        if (parameter.getName() != null) {
+            criteria.createAlias("empData", "ce", JoinType.INNER_JOIN);
+            criteria.createAlias("ce.bioData", "bio", JoinType.INNER_JOIN);
+            criteria.add(Restrictions.ilike("bio.combineName", parameter.getName().toLowerCase(), MatchMode.ANYWHERE));
+        }
+        if (parameter.getNik() != null) {
+            criteria.createAlias("empData", "ce", JoinType.INNER_JOIN);
+            criteria.add(Restrictions.like("ce.nik", parameter.getNik(), MatchMode.ANYWHERE));
+
+        }
+        if (parameter.getNoJab() != null) {
+            criteria.createAlias("empData", "ce", JoinType.INNER_JOIN);
+            criteria.createAlias("ce.jabatanByJabatanId", "jb", JoinType.INNER_JOIN);
+            criteria.add(Restrictions.like("jb.code", parameter.getNoJab(), MatchMode.ANYWHERE));
+
+        }
+        criteria.add(Restrictions.isNotNull("id"));
+
+    }
 }
--- inkubator-hrm/src/main/java/com/inkubator/hrm/web/search/TempAttendanceRealizationSearchParameter.java
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/web/search/TempAttendanceRealizationSearchParameter.java
@@ -1,21 +1,69 @@
 package com.inkubator.hrm.web.search;
+
 import com.inkubator.webcore.util.SearchParameter;
 import org.apache.commons.lang3.StringUtils;
+
 /**
  *
  * @author WebGenX
  */
-public class TempAttendanceRealizationSearchParameter extends SearchParameter{
-private String createdBy;
-public String getCreatedBy(){
-if (StringUtils.equalsIgnoreCase(getKeyParam(),"createdBy")){
-createdBy = getParameter();
-} else {
-createdBy = null;
-}
-return createdBy;
-}
-public void setCreatedBy(String createdBy){
-this.createdBy = createdBy;
-}
+public class TempAttendanceRealizationSearchParameter extends SearchParameter {
+
+    private String nik;
+    private String name;
+    private String noJab;
+    private String jabatan;
+
+    public String getNik() {
+        if (StringUtils.equalsIgnoreCase(getKeyParam(), "nik")) {
+            nik = getParameter();
+        } else {
+            nik = null;
+        }
+        return nik;
+    }
+
+    public void setNik(String nik) {
+        this.nik = nik;
+    }
+
+    public String getName() {
+        if (StringUtils.equalsIgnoreCase(getKeyParam(), "name")) {
+            name = getParameter();
+        } else {
+            name = null;
+        }
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNoJab() {
+        if (StringUtils.equalsIgnoreCase(getKeyParam(), "noJab")) {
+            noJab = getParameter();
+        } else {
+            noJab = null;
+        }
+        return noJab;
+    }
+
+    public void setNoJab(String noJab) {
+        this.noJab = noJab;
+    }
+
+    public String getJabatan() {
+        if (StringUtils.equalsIgnoreCase(getKeyParam(), "jabatan")) {
+            jabatan = getParameter();
+        } else {
+            jabatan = null;
+        }
+        return jabatan;
+    }
+
+    public void setJabatan(String jabatan) {
+        this.jabatan = jabatan;
+    }
+
 }
