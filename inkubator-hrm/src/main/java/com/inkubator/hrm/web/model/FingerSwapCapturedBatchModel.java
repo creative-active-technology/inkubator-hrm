57edaa4c48620de4ffeb1baa11848c7deaadbbd0
rizkykojek <rizkykojek@gmail.com>
2015-07-07 08:53:39 +0700

    Bug #1044 - Batch proses untuk waktu kerja - create scheduler untuk meproses : 1. ambil data dari mesin 2. sinkronisasi data finger 3. kalkulasi data finger

--- inkubator-hrm/src/main/java/com/inkubator/hrm/web/model/FingerSwapCapturedBatchModel.java
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/web/model/FingerSwapCapturedBatchModel.java
@@ -5,6 +5,7 @@
 package com.inkubator.hrm.web.model;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -19,6 +20,7 @@ public class FingerSwapCapturedBatchModel implements Serializable{
 	private String fingerIndexId;
     private String dateTime;
     private String createdBy;
+    private Date createdOn;
     private Long machineId;
 	
     @XmlElement(name = "PIN")
@@ -47,6 +49,11 @@ public class FingerSwapCapturedBatchModel implements Serializable{
 	public void setMachineId(Long machineId) {
 		this.machineId = machineId;
 	}
-	
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}	
 	
 }
