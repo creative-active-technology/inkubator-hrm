110cac7fb7d90ca03f676bae9fc387d1eb8a8844
dicky <dicky@inkuba-laptop>
2015-08-21 10:40:58 +0700

    Initial Commit #1194 Revisi FSD Jabatan (MASTER)

---
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/dao/JabatanProfesiDao.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.inkubator.hrm.dao;
+
+import com.inkubator.datacore.dao.IDAO;
+
+import com.inkubator.hrm.entity.JabatanProfesi;
+
+import java.util.List;
+
+/**
+ *
+ * @author Ahmad Mudzakkir Amal
+ */
+public interface JabatanProfesiDao extends IDAO<JabatanProfesi>{
+    
+}
---
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/web/model/JobJabatanModel.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.inkubator.hrm.web.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Ahmad Mudzakkir Amal
+ */
+public class JobJabatanModel implements Serializable {
+
+    private Long id;
+    private Long jabatanAtasanId;
+    private String kodeJabatan;
+    private String namaJabatan;
+    private Long golonganJabatanId;
+    private Long unitKerjaId;
+    private Long departementId;
+    private Long posBiayaId;
+    private String tujuanJabatan;
+    private Long salaryGradeId;
+    private String NIK;
+//    private Integer levelJabatan;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getJabatanAtasanId() {
+        return jabatanAtasanId;
+    }
+
+    public void setJabatanAtasanId(Long jabatanAtasanId) {
+        this.jabatanAtasanId = jabatanAtasanId;
+    }
+
+    public String getKodeJabatan() {
+        return kodeJabatan;
+    }
+
+    public void setKodeJabatan(String kodeJabatan) {
+        this.kodeJabatan = kodeJabatan;
+    }
+
+    public String getNamaJabatan() {
+        return namaJabatan;
+    }
+
+    public void setNamaJabatan(String namaJabatan) {
+        this.namaJabatan = namaJabatan;
+    }
+
+    public Long getGolonganJabatanId() {
+        return golonganJabatanId;
+    }
+
+    public void setGolonganJabatanId(Long golonganJabatanId) {
+        this.golonganJabatanId = golonganJabatanId;
+    }
+
+    public Long getUnitKerjaId() {
+        return unitKerjaId;
+    }
+
+    public void setUnitKerjaId(Long unitKerjaId) {
+        this.unitKerjaId = unitKerjaId;
+    }
+
+    public Long getDepartementId() {
+        return departementId;
+    }
+
+    public void setDepartementId(Long departementId) {
+        this.departementId = departementId;
+    }
+
+    public Long getPosBiayaId() {
+        return posBiayaId;
+    }
+
+    public void setPosBiayaId(Long posBiayaId) {
+        this.posBiayaId = posBiayaId;
+    }
+
+    public String getTujuanJabatan() {
+        return tujuanJabatan;
+    }
+
+    public void setTujuanJabatan(String tujuanJabatan) {
+        this.tujuanJabatan = tujuanJabatan;
+    }
+
+    //    public Integer getLevelJabatan() {
+    //        return levelJabatan;
+    //    }
+    //
+    //    public void setLevelJabatan(Integer levelJabatan) {
+    //        this.levelJabatan = levelJabatan;
+    //    }
+    public Long getSalaryGradeId() {
+        return salaryGradeId;
+    }
+
+    public void setSalaryGradeId(Long salaryGradeId) {
+        this.salaryGradeId = salaryGradeId;
+    }
+
+    public String getNIK() {
+        return NIK;
+    }
+
+    public void setNIK(String NIK) {
+        this.NIK = NIK;
+    }
+    
+}
