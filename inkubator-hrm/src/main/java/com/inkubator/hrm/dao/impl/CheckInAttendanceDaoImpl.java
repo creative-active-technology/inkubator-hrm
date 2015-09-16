fe48ee92c964f7f83469405bd31ccc047a02b896
cihuy12345 <cihuyclashofclans@gmail.com>
2015-09-16 10:59:50 +0700

    fix search check in attendance

--- inkubator-hrm/src/main/java/com/inkubator/hrm/dao/impl/CheckInAttendanceDaoImpl.java
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/dao/impl/CheckInAttendanceDaoImpl.java
@@ -5,15 +5,13 @@
  */
 package com.inkubator.hrm.dao.impl;
 
-import com.inkubator.datacore.dao.impl.IDAOImpl;
-import com.inkubator.hrm.dao.CheckInAttendanceDao;
-import com.inkubator.hrm.entity.CheckInAttendance;
-import com.inkubator.hrm.web.search.CheckInAttendanceSearchParameter;
 import java.util.Date;
 import java.util.List;
+
 import org.hibernate.Criteria;
 import org.hibernate.FetchMode;
 import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Disjunction;
 import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.ProjectionList;
@@ -23,6 +21,11 @@ import org.hibernate.criterion.Restrictions;
 import org.hibernate.sql.JoinType;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Repository;
+
+import com.inkubator.datacore.dao.impl.IDAOImpl;
+import com.inkubator.hrm.dao.CheckInAttendanceDao;
+import com.inkubator.hrm.entity.CheckInAttendance;
+import com.inkubator.hrm.web.search.CheckInAttendanceSearchParameter;
 
 /**
  *
@@ -57,11 +60,18 @@ public class CheckInAttendanceDaoImpl extends IDAOImpl<CheckInAttendance> implem
     }
 
     private void doSearchByParam(CheckInAttendanceSearchParameter searchParameter, Criteria criteria) {
-
         if (searchParameter.getEmpData() != null) {
-            criteria.add(Restrictions.like("empData", searchParameter.getEmpData(), MatchMode.START));
+        	criteria.createAlias("empData", "empData", JoinType.INNER_JOIN);
+        	criteria.createAlias("empData.bioData", "bioData", JoinType.INNER_JOIN);
+        	Disjunction disjunction = Restrictions.disjunction();
+        	disjunction.add(Restrictions.like("bioData.firstName", searchParameter.getEmpData(), MatchMode.START));
+        	disjunction.add(Restrictions.like("bioData.lastName", searchParameter.getEmpData(), MatchMode.START));
+        	criteria.add(disjunction);
         }
 
+        if (searchParameter.getIpAddress() != null) {
+        	criteria.add(Restrictions.like("ipAddress", searchParameter.getIpAddress(), MatchMode.ANYWHERE));
+        }
         criteria.add(Restrictions.isNotNull("id"));
     }
 
