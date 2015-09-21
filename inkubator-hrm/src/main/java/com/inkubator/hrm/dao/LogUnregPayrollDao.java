85f27ca3bceb4806d3f170e697e2d20a88554d3c
rizkykojek <rizkykojek@gmail.com>
2015-09-21 14:21:40 +0700

    Bug #1365 - lengkapi kekurangan pada halaman pembayaran pendapatan tidak tetap (OHPQ) 1. tambahkan kolom jumlah yang harus dibayarkan dan karyawan penerima 2. tambahkan komponen yang tercantum pada pendapatan tidak tetap

--- inkubator-hrm/src/main/java/com/inkubator/hrm/dao/LogUnregPayrollDao.java
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/dao/LogUnregPayrollDao.java
@@ -21,6 +21,8 @@ public interface LogUnregPayrollDao extends IDAO<LogUnregPayroll> {
 	public Long getTotalEmployeeByUnregSalaryId(Long unregSalaryId);
 	
 	public BigDecimal getTotalTakeHomePayByUnregSalaryId(Long unregSalaryId);
+	
+	public List<LogUnregPayroll> getAllDataByEmpDataIdAndUnregSalaryId(Long empDataId, Long unregSalaryId);
 
 	public List<UnregPayrollViewModel> getByParam(UnregPayrollSearchParameter parameter, int firstResult, int maxResults, Order orderable);
 
