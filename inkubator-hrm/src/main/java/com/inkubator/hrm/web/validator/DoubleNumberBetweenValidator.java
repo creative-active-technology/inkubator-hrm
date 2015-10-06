b4c853ce6acf3166db723dd7e8b25a9c17424363
rizkykojek <rizkykojek@gmail.com>
2015-10-06 10:38:56 +0700

    Update : make dynamic DoubleValidator and IntegerValidator

--- inkubator-hrm/src/main/java/com/inkubator/hrm/web/validator/DoubleNumberBetweenValidator.java
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/web/validator/DoubleNumberBetweenValidator.java
@@ -8,6 +8,10 @@ import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 
+import org.apache.commons.lang3.StringUtils;
+
+import com.inkubator.hrm.HRMConstant;
+
 /**
  *
  * @author Deni Husni FR
@@ -28,10 +32,23 @@ public class DoubleNumberBetweenValidator implements Validator {
             return; // Let required="true" do its job.
         }
 
+        // get required attributes value.
+        String constraint = (String) component.getAttributes().get("validatorConstraint");
+        String validatorMessage = (String) component.getAttributes().get("validatorMessage");
+        
         // Check if begin time is bigger/equal with end time
-        Double endNumber = Double.parseDouble(data);
-        if (beginNumber >= endNumber) {
-            String validatorMessage = (String) component.getAttributes().get("validatorMessage");
+        double endNumber = Double.parseDouble(data);
+        if (StringUtils.equals(constraint, HRMConstant.COMPARATOR_EQUALS) && beginNumber == endNumber) {
+            throw new ValidatorException(new FacesMessage(validatorMessage));
+        } else if (StringUtils.equals(constraint, HRMConstant.COMPARATOR_NOT_EQUALS) && beginNumber != endNumber) {
+            throw new ValidatorException(new FacesMessage(validatorMessage));
+        } else if (StringUtils.equals(constraint, HRMConstant.COMPARATOR_GREATER_EQUALS) && beginNumber >= endNumber) {
+            throw new ValidatorException(new FacesMessage(validatorMessage));
+        } else if (StringUtils.equals(constraint, HRMConstant.COMPARATOR_GREATER_THAN) && beginNumber > endNumber) {
+            throw new ValidatorException(new FacesMessage(validatorMessage));
+        } else if (StringUtils.equals(constraint, HRMConstant.COMPARATOR_LESS_EQUALS) && beginNumber <= endNumber) {
+            throw new ValidatorException(new FacesMessage(validatorMessage));
+        } else if (StringUtils.equals(constraint, HRMConstant.COMPARATOR_LESS_THAN) && beginNumber < endNumber) {
             throw new ValidatorException(new FacesMessage(validatorMessage));
         }
     }
