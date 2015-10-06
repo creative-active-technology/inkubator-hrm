718edbfacf788ef0425012373c42b408132e7aca
dickyamja <amjadicky@gmail.com>
2015-10-06 14:44:57 +0700

    Commit Task Evaluation #1277 format email setelah dilakukan reset password belum sesuai

--- inkubator-hrm/src/main/java/com/inkubator/hrm/service/impl/CheckPasswordHistoryEmailNotSendServiceImpl.java
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/service/impl/CheckPasswordHistoryEmailNotSendServiceImpl.java
@@ -17,10 +17,13 @@ import com.inkubator.hrm.HRMConstant;
 import com.inkubator.hrm.dao.PasswordHistoryDao;
 import com.inkubator.hrm.entity.PasswordHistory;
 import com.inkubator.hrm.service.CheckPasswordHistoryEmailNotSendService;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import org.hibernate.criterion.Order;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -53,6 +56,8 @@ public class CheckPasswordHistoryEmailNotSendServiceImpl extends IServiceImpl im
         LOGGER.info("--------------- TOTAL sending email which will be processed : " + passwordHistories.size());
         for (PasswordHistory passwordHistory : passwordHistories) {
 //            try {
+        		SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy");
+        		SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm:ss");
                 //decrypt the password before sending email
                 String passwordDecrypted = AESUtil.getAESDescription(passwordHistory.getPassword(), HRMConstant.KEYVALUE, HRMConstant.AES_ALGO);
 //                passwordHistory.setPassword(passwordDecrypted);
@@ -82,8 +87,14 @@ public class CheckPasswordHistoryEmailNotSendServiceImpl extends IServiceImpl im
                         maptoSend.put("headerInfo", "Your account in OPTIMA HR already created.<br/>");
                     }
                     
+                    //Password reset use different email template
                     if (pass.getRequestType().equalsIgnoreCase(HRMConstant.USER_RESET)) {
-                        maptoSend.put("headerInfo", "Your Password in OPTIMA HR has been reset. <br/>");
+                    	String headerInfo = "On " + dateFormat.format(passwordHistory.getCreatedOn()) 
+                    			+ " Time " + timeFormat.format(passwordHistory.getCreatedOn()) 
+                    			+ " you or someone else has been asked to reset the password for your account at OPTIMA-HR Application. <br/>";
+                        maptoSend.put("headerInfo", headerInfo);
+                        vtm.setTemplatePath("email_user_password_reset_en.vm");
+                        vtm.setSubject("Password Reset at OPTIMA-HR Application");
                     }
                 }
                 if (pass.getLocalId().equals("in")) {
@@ -95,8 +106,14 @@ public class CheckPasswordHistoryEmailNotSendServiceImpl extends IServiceImpl im
                         maptoSend.put("headerInfo", "Anda telah terdaftar di Aplikasi OPTIMA HR <br/>");
                     }
                     
+                    //Password reset use different email template
                     if (pass.getRequestType().equalsIgnoreCase(HRMConstant.USER_RESET)) {
-                        maptoSend.put("headerInfo", "Password Anda pada Aplikasi OPTIMA HR berhasil direset. <br/>");
+                    	String headerInfo = "Pada Tanggal " + dateFormat.format(passwordHistory.getCreatedOn()) 
+                    			+ " Waktu " + timeFormat.format(passwordHistory.getCreatedOn()) 
+                    			+ " Anda atau seseorang telah meminta diresetkan password untuk account anda di aplikasi OPTIMA-HR. <br/>";
+                        maptoSend.put("headerInfo", headerInfo);
+                        vtm.setTemplatePath("email_user_password_reset.vm");
+                        vtm.setSubject("Reset Password di aplikasi OPTIMA-HR");
                     }
                 }
                 Gson gson = new GsonBuilder().create();
@@ -288,6 +305,9 @@ public class CheckPasswordHistoryEmailNotSendServiceImpl extends IServiceImpl im
     }
     
     private void passwordHistorySendingEmail(PasswordHistory passwordHistory, String pass) throws Exception {
+    	
+    	SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy");
+		SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm:ss");
         VelocityTempalteModel vtm = new VelocityTempalteModel();
         List<String> toSend = new ArrayList<>();
         List<String> toSentCC = new ArrayList<>();
@@ -308,8 +328,14 @@ public class CheckPasswordHistoryEmailNotSendServiceImpl extends IServiceImpl im
                 maptoSend.put("headerInfo", "Your account in OPTIMA HR already created.<br/>");
             }
             
+            //Password reset use different email template
             if (passwordHistory.getRequestType().equalsIgnoreCase(HRMConstant.USER_RESET)) {
-                maptoSend.put("headerInfo", "Your Password in OPTIMA HR has been reset. <br/>");
+            	String headerInfo = "On " + dateFormat.format(passwordHistory.getCreatedOn()) 
+            			+ " Time " + timeFormat.format(passwordHistory.getCreatedOn()) 
+            			+ " you or someone else has been asked to reset the password for your account at OPTIMA-HR Application. <br/>";
+                maptoSend.put("headerInfo", headerInfo);
+                vtm.setTemplatePath("email_user_password_reset_en.vm");
+                vtm.setSubject("Password Reset at OPTIMA-HR Application");
             }
         }
         if (passwordHistory.getLocalId().equals("in")) {
@@ -321,8 +347,14 @@ public class CheckPasswordHistoryEmailNotSendServiceImpl extends IServiceImpl im
                 maptoSend.put("headerInfo", "Anda telah terdaftar di Aplikasi OPTIMA HR <br/>");
             }
             
+            //Password reset use different email template
             if (passwordHistory.getRequestType().equalsIgnoreCase(HRMConstant.USER_RESET)) {
-                maptoSend.put("headerInfo", "Password Anda pada Aplikasi OPTIMA HR berhasil direset. <br/>");
+            	String headerInfo = "Pada Tanggal " + dateFormat.format(passwordHistory.getCreatedOn()) 
+            			+ " Waktu " + timeFormat.format(passwordHistory.getCreatedOn()) 
+            			+ " Anda atau seseorang telah meminta diresetkan password untuk account anda di aplikasi OPTIMA-HR. <br/>";
+                maptoSend.put("headerInfo", headerInfo);
+                vtm.setTemplatePath("email_user_password_reset.vm");
+                vtm.setSubject("Reset Password di aplikasi OPTIMA-HR");
             }
         }
         Gson gson = new GsonBuilder().create();
