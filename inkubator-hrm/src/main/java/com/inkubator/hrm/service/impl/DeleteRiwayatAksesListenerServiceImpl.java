4e078e74fb9982d1523c07362915cbaec226f5b2
denifahri <denifahri@positron>
2015-10-07 11:02:15 +0700

    Jsmtemplade Schedulle

---
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/service/impl/DeleteRiwayatAksesListenerServiceImpl.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.inkubator.hrm.service.impl;
+
+import com.inkubator.hrm.dao.RiwayatAksesDao;
+import com.inkubator.hrm.entity.RiwayatAkses;
+import com.inkubator.hrm.entity.SchedulerConfig;
+import com.inkubator.hrm.entity.SchedulerLog;
+import java.util.List;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author Deni Husni FR
+ */
+public class DeleteRiwayatAksesListenerServiceImpl extends BaseSchedulerDinamicListenerImpl implements MessageListener {
+
+    private int difWeekToDelete;
+    @Autowired
+    private RiwayatAksesDao riwayatAksesDao;
+
+    @Override
+    @Transactional(readOnly = false, propagation = Propagation.REQUIRES_NEW, isolation = Isolation.READ_COMMITTED, rollbackFor = Exception.class)
+    public void onMessage(Message msg) {
+        try {
+            TextMessage textMessage = (TextMessage) msg;
+            SchedulerLog schedulerLog = new SchedulerLog();
+            schedulerLog.setSchedulerConfig(new SchedulerConfig(Long.parseLong(textMessage.getText())));
+            SchedulerLog log = super.doSaveSchedulerLogSchedulerLog(schedulerLog);
+            deleteRiwayatAkses();
+            log.setStatusMessages("FINISH");
+            schedulerLogDao.update(log);
+        } catch (Exception ex) {
+            LOGGER.error(ex, ex);
+        }
+    }
+
+    private void deleteRiwayatAkses() throws Exception {
+        LOGGER.error("Begin Running Dellete Riwayar Akses");
+        List<RiwayatAkses> dataToDelete = riwayatAksesDao.getByWeekDif(difWeekToDelete);
+        LOGGER.error("Ukuran Data to Delete " + dataToDelete.size());
+        riwayatAksesDao.deleteBatch(dataToDelete);
+        LOGGER.error("Finish Running Dellete Riwayar Akses");
+    }
+
+    public void setDifWeekToDelete(int difWeekToDelete) {
+        this.difWeekToDelete = difWeekToDelete;
+    }
+}
