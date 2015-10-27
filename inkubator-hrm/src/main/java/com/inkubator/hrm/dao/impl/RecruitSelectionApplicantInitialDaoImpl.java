6d056a436557c817a73a25790f000ef127aaf465
cihuy12345 <cihuyclashofclans@gmail.com>
2015-10-27 10:31:09 +0700

    Merge origin/master
    Conflicts:
    	inkubator-hrm/src/main/java/com/inkubator/hrm/dao/RecruitSelectionApplicantInitialDao.java
    	inkubator-hrm/src/main/java/com/inkubator/hrm/dao/impl/RecruitSelectionApplicantInitialDaoImpl.java
    	inkubator-hrm/src/main/java/com/inkubator/hrm/entity/RecruitApplicant.java
    	inkubator-hrm/src/main/java/com/inkubator/hrm/entity/RecruitSelectionApplicantInitial.java
    	inkubator-hrm/src/main/resources/Messages.properties
    	inkubator-hrm/src/main/resources/Messages_en.properties
    	inkubator-hrm/src/main/resources/Messages_in.properties

--- inkubator-hrm/src/main/java/com/inkubator/hrm/dao/impl/RecruitSelectionApplicantInitialDaoImpl.java
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/dao/impl/RecruitSelectionApplicantInitialDaoImpl.java
@@ -3,13 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.inkubator.hrm.dao.impl;
 
 import com.inkubator.datacore.dao.impl.IDAOImpl;
 import com.inkubator.hrm.dao.RecruitSelectionApplicantInitialDao;
 import com.inkubator.hrm.entity.RecruitSelectionApplicantInitial;
-
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Repository;
 
@@ -25,5 +23,4 @@ public class RecruitSelectionApplicantInitialDaoImpl extends IDAOImpl<RecruitSel
     public Class<RecruitSelectionApplicantInitial> getEntityClass() {
         return RecruitSelectionApplicantInitial.class;
     }
-
 }
