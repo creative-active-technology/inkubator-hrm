e2f9e79779e896e98104f81ddfe169c679a13f21
rizkykojek <rizkykojek@gmail.com>
2015-11-26 09:34:10 +0700

    Fixed, validation name on BioData

--- inkubator-hrm/src/main/java/com/inkubator/hrm/web/model/BioDataModel.java
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/web/model/BioDataModel.java
@@ -102,7 +102,7 @@ public class BioDataModel implements Serializable {
         this.maritalStatusId = maritalStatusId;
     }
 
-    @Pattern(regexp = "^[A-Za-z]+", message = "{errorr_first_name}")
+    @Pattern(regexp = "^[A-Za-z][A-Za-z\\s]+", message = "{errorr_first_name}")
     public String getFirstName() {
         return firstName;
     }
@@ -111,7 +111,7 @@ public class BioDataModel implements Serializable {
         this.firstName = firstName;
     }
 
-    @Pattern(regexp = "^([A-Za-z]|)+", message = "{errorr_last_name}")
+    @Pattern(regexp = "^([A-Za-z][A-Za-z\\s]|)+", message = "{errorr_last_name}")
     public String getLastName() {
         return lastName;
     }
