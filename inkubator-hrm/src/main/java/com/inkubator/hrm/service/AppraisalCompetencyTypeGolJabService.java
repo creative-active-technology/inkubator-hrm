9939d25884a25483c3003edac6162be76d095c0e
dickyamja <amjadicky@gmail.com>
2015-12-04 13:17:54 +0700

    #1648 FSD Tipe Kompetensi
    (Initial Develop Form Page)

--- inkubator-hrm/src/main/java/com/inkubator/hrm/service/AppraisalCompetencyTypeGolJabService.java
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/service/AppraisalCompetencyTypeGolJabService.java
@@ -1,5 +1,7 @@
 package com.inkubator.hrm.service;
 
+import java.util.List;
+
 import com.inkubator.datacore.service.IService;
 import com.inkubator.hrm.entity.AppraisalCompetencyTypeGolJab;
 
@@ -7,5 +9,5 @@ import com.inkubator.hrm.entity.AppraisalCompetencyTypeGolJab;
 * @author Ahmad Mudzakkir Amal
 */
 public interface AppraisalCompetencyTypeGolJabService extends IService<AppraisalCompetencyTypeGolJab> {
-	
+	public List<AppraisalCompetencyTypeGolJab> getListByAppraisalCompetenceTypeId(Long appraisalCompetenceTypeId) throws Exception;
 }
