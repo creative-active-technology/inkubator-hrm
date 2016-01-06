5302cf378679022ccc54875ea998a9c35fb34d04
denifahri <denifahri@positron>
2016-01-06 10:05:05 +0700

    Bug -Bug Pencarian Nama

--- inkubator-hrm/src/main/java/com/inkubator/hrm/dao/impl/JabatanDaoImpl.java
+++ inkubator-hrm/src/main/java/com/inkubator/hrm/dao/impl/JabatanDaoImpl.java
@@ -104,6 +104,10 @@ public class JabatanDaoImpl extends IDAOImpl<Jabatan> implements JabatanDao {
         if (StringUtils.isNotEmpty(parameter.getCode())) {
             criteria.add(Restrictions.like("code", parameter.getCode(), MatchMode.ANYWHERE));
         }
+        
+         if (StringUtils.isNotEmpty(parameter.getGolJab())) {
+            criteria.add(Restrictions.like("golonganJabatan.code", parameter.getGolJab(), MatchMode.ANYWHERE));
+        }
         if (StringUtils.isNotEmpty(parameter.getName())) {
             criteria.add(Restrictions.like("name", parameter.getName(), MatchMode.ANYWHERE));
         }
